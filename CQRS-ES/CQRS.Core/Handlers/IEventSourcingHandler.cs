cf66855e189f2b13c8113a74b7a77045796ea406
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-15 17:51:26 +0700

    ch06-8. EventSourcingHandler

--- CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
+++ CQRS-ES/CQRS.Core/Handlers/IEventSourcingHandler.cs
@@ -5,6 +5,6 @@ namespace CQRS.Core.Handlers
     public interface IEventSourcingHandler<T>
     {
         Task SaveAsync(AggregateRoot aggregate);
-        Task<T> GetByIdAsync(Guid id);
+        Task<T> GetByIdAsync(Guid aggregateId);
     }
 }
