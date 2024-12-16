adfcbac89d01492ff9120de1c7cfcab2915fe86b
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-16 07:39:02 +0700

    ch07-3. CommandHandler (The Concrete Colleague)

--- SM-POST/POST.Cmd/Post.Cmd.Api/Commands/EditCommentCommand.cs
+++ SM-POST/POST.Cmd/Post.Cmd.Api/Commands/EditCommentCommand.cs
@@ -10,5 +10,6 @@ namespace Post.Cmd.Api.Commands
     {
         public Guid CommentId { get; set; }
         public string Comment { get; set; }
+        public string Username { get; set; }
     }
 }
