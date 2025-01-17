905746270c60f293c31ae6aa09bc7eb91710b391
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-01-17 09:23:08 +0700

    fixbug 2

--- SM-POST/POST.Query/Post.Query.Api/Program.cs
+++ SM-POST/POST.Query/Post.Query.Api/Program.cs
@@ -10,7 +10,7 @@ using Post.Query.Infrastructure.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-Action<DbContextOptionsBuilder> configureDbContext = (o => o.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+Action<DbContextOptionsBuilder>configureDbContext=(options=>options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
 builder.Services.AddDbContext<DatabaseContext>(configureDbContext);
 builder.Services.AddSingleton<DatabaseContextFactory>(new DatabaseContextFactory(configureDbContext));
 
